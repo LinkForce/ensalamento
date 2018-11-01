e5b260c0ee2a827b59f5da3a0372b506dc9c5b09
Matheus Horstmann <mch15@inf.ufpr.br>
2018-11-01 11:23:38 -0300

    #16: Create AutoUpdate

---
+++ common/models/models.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var models = [
+        'User',
+        'AccessToken',
+        'ACL',
+        'RoleMapping',
+        'Role',
+        'Sala',
+        'Bloco',
+        'Disciplina',
+        'Equivalenciadisciplina',
+        'Evento',
+        'Turma',
+        'Recursodesala',
+        'SalaRecursodesala',
+        'DisciplinaRecursodesala',
+        'Tipodesala'
+    ];
+
+exports.models = models;
